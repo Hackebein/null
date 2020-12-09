42311ab75ec7f6e073f9a236918d41343d74b857
Lars Olzem <hackebein@gmail.com>
2020-12-09 17:10:03 +0100

    next try

---
+++ node/index.js
@@ -0,0 +1,26 @@
+const fs = require('fs');
+const log = new Logger();
+
+
+const binary = process.argv.shift();
+const file = process.argv.shift();
+const output = process.argv.shift();
+const repo = process.argv.shift();
+
+fs.writeFile(output, yaml.dump({
+    version: '2.4',
+    services: {
+        latest: {
+            image: repo + ':latest',
+            build: {
+                context: ".",
+                dockerfile: "Dockerfile"
+            },
+        },
+    },
+}), (err) => {
+    if(err) {
+        log.error(err);
+    }
+    log.info('job done');
+});
